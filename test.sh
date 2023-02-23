f37841ed217588ec9c59727cfc46da4af3391274
f2432 <109317080+f2432@users.noreply.github.com>
2023-02-23 14:44:41 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 echo "A executar os testes ..."
 echo
 
-saida=$(echo -e "90\n1.75\n" | ./a.out)
-esperado="O teu índice de massa corporal é 29,387."
+saida=$(echo -e "20\n10\n23\n | ./a.out)
+esperado="O valor do produto final é 38,1 Euros."
 
 if [ "$saida" != "$esperado" ]; then
     echo "Teste falhou: a saída do programa é diferente do esperado"
